65b0b06f1f76cad1dd7295868cbcc84685c963a0
Larissa <tchanilara@gmail.com>
2023-09-18 15:20:25 -0400

    ignore idea files and add main method

---
+++ MyFirstJavaProject/src/myfirstjavaproject/MyFirstJavaClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package myfirstjavaproject;
+
+/**
+ * 
+ */
+public class MyFirstJavaClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
