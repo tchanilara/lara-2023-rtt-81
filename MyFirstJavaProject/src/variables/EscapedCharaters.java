b0c1cc2cba3e35f94a3ca7f20fb16add5533312e
Larissa <tchanilara@gmail.com>
2023-09-19 14:22:46 -0400

    add scanner class examples and escaped variable

---
+++ MyFirstJavaProject/src/variables/EscapedCharaters.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class EscapedCharaters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a tab\r charater ->\t <-");
+		System.out.println("form feed \f");
+		System.out.println("This is a tab\r charater ->\t <-");
+
+	}
+
+}
---
+++ MyFirstJavaProject/src/variables/ScannerExample.java
@@ -0,0 +1,21 @@
+package variables;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+	public static void main(String[] args) {
+
+		System.out.print("Enter your name: ");
+		// create an Object of Scanner class
+		Scanner input = new Scanner(System.in);
+		
+		// read input(line of text) from the keyboard
+		String name = input.nextLine();
+		
+		// prints the name
+		System.out.println("My name is " + name);
+		// closes the scanner
+		input.close();
+	}
+
+}
