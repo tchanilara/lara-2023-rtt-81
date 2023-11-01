54f87ad47d9f8282c66d7afd2e4cd0203cfae384
PSNY9ZH3Z146\Learner_9ZH3Z146 <tchanilara@gmail.com>
2023-11-01 15:13:55 -0400

    sprint boot project

---
+++ sprintboot/src/main/java/org/perscholas/sprintboot/SprintbootApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootApplication.class, args);
+	}
+
+}
---
+++ sprintboot/src/test/java/org/perscholas/sprintboot/SprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.sprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
