5079e544090ab40b8fb48c6732c41df28acbb639
PSNY9ZH3Z146\Learner_9ZH3Z146 <tchanilara@gmail.com>
2023-11-01 15:13:20 -0400

    worked on School management system SBA

---
+++ SMS/src/main/java/jpa/entitymodels/Course.java
@@ -0,0 +1,40 @@
+package jpa.entitymodels;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name= "courses")
+public class Course {
+
+	public Course() {
+
+	}
+
+	public Course(String cInstructor, String cName) {
+		this.cInstructor = cInstructor;
+		this.cName = cName;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private @Getter @Setter Integer cId;
+	
+	@Override
+	public String toString() {
+		return "Course [cId=" + cId + ", cInstructor=" + cInstructor + ", cName=" + cName + "]";
+	}
+
+	@Column(name = "cInstructorName")
+	private @Getter @Setter String cInstructor;
+	
+	@Column(name = "cName")
+	private @Getter @Setter String cName;
+}
